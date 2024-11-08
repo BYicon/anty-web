8cf7ab184e14ad51365936f02e27dfa50c143a14
ioby <mt130827@163.com>
2024-11-09 00:15:03 +0800

    feat:add Nav

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,6 +9,7 @@ import ThemeToggle from "../theme-toggle/theme-toggle";
 import { useCommonStore } from "@/stores/useStore";
 import "./header.scss";
 import { EnumTheme } from "@/shared/constant/enums";
+import Link from "next/link";
 
 console.log('process.env', process.env.NODE_ENV);
 const Header = () => {
@@ -48,12 +49,17 @@ const Header = () => {
   }, []);
 
   return (
-    <nav className="main-nav">
+    <header className="main-header">
       <div className="text-2xl z-10 max-w-5xl items-center justify-between font-mono text-sm flex fs-20">
         <img src="/images/logo.png" alt="logo" className={`w-10 h-10 border rounded-full ${theme === EnumTheme.Light ? '' : 'filter invert'}`} />
-        <span className="text-sm ml-2 font-bold">Anty | MirrorCoin</span>
+        <span className="text-sm ml-2">Anty｜MirrorCoin</span>
       </div>
       <div className="flex items-center gap-4">
+        <nav className="main-nav">
+          <Link href="/" className="hover:text-blue-500">Home</Link>
+          <Link href="/recharge" className="hover:text-blue-500">Recharge</Link>
+          <Link href="/about" className="hover:text-blue-500">About</Link>
+        </nav>
         <ThemeToggle />
         {account ? (
           <span className="text-sm">
@@ -68,7 +74,7 @@ const Header = () => {
           </button>
         )}
       </div>
-    </nav>
+    </header>
   );
 };
 
