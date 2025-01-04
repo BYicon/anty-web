8c4036ad47c051ff996b7b8822911040b03693aa
ioby <mt130827@163.com>
2025-01-04 22:32:28 +0800

    feat:update etf styles

--- src/app/etf/page.tsx
+++ src/app/etf/page.tsx
@@ -9,26 +9,28 @@ import {
 } from "@/components/ui/card";
 import Portfolio from "./portfolio";
 import Info from "./info";
-import Right from "./right";
+import Operate from "./operate";
 
 export default function ETFPage() {
   const { address: currentAddress } = useAccount();
 
   return (
-    <div className="common-page flex gap-4">
-      <Card className="flex-1">
-        <CardHeader>
-          <CardTitle>MIR ETF</CardTitle>
-          <CardDescription>make crypto simple</CardDescription>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-4">
-          <Info />  
-          <Portfolio />
-        </CardContent>
-      </Card>
-      <Card className="w-[600px]">
-        <Right />
-      </Card>
+    <div className="common-page">
+      <div className="flex gap-4 w-[1400px] mx-auto">
+        <Card className="flex-1">
+          <CardHeader>
+            <CardTitle>MIR ETF</CardTitle>
+            <CardDescription>make crypto simple</CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-4">
+            <Info />
+            <Portfolio />
+          </CardContent>
+        </Card>
+        <div className="w-[500px]">
+          <Operate />
+        </div>
+      </div>
     </div>
   );
 }
